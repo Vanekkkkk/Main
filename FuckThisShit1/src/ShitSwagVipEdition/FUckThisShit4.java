35552f946fb58ed4fc25f451949bd566a5694076
Vanekkkkk <semenov.vanya@gmail.com>
2014-10-10 03:23:39 +0300

    Second

--- FuckThisShit1/src/ShitSwagVipEdition/FUckThisShit4.java
+++ FuckThisShit1/src/ShitSwagVipEdition/FUckThisShit4.java
@@ -18,28 +18,30 @@ public class FUckThisShit4 {
         if(scan.hasNextInt()){
             c = scan.nextInt();
         } else System.out.println("Wrong, BITCH!");
-        int[] x={a,b,c};
+            if(a != b || a != c || b != c){
+            int[] x={a,b,c};
 
-        for(int i=0;i<x.length;i++){
-            int min= x[i], index= i;
-            for(int j = i+1;j<x.length;j++){
-                if(x[j]<min){
-                    min = x[j];
-                    index = j;
+            for(int i=0;i<x.length;i++){
+                int min= x[i], index= i;
+                for(int j = i+1;j<x.length;j++){
+                    if(x[j]<min){
+                        min = x[j];
+                        index = j;
+                    }
+
                 }
-
+                if(i != index){
+                    int buf = x[i];
+                    x[i] = x[index];
+                    x[index] = buf;
+                }
             }
-            if(i != index){
-                int buf = x[i];
-                x[i] = x[index];
-                x[index] = buf;
-            }
-        }
-        a = x[0];
-        b = x[1];
-        c = x[2];
-        System.out.println("Here's your numbers, BITCH!");
-        System.out.println("a= " + a + "\n" + "b= " + b + "\n" + "c= " + c);
-        System.out.println("WHERE IS MY MONEY, BITCH?!");
+            a = x[0];
+            b = x[1];
+            c = x[2];
+            System.out.println("Here's your numbers, BITCH!");
+            System.out.println("a= " + a + "\n" + "b= " + b + "\n" + "c= " + c);
+            System.out.println("WHERE IS MY MONEY, BITCH?!");
+            } else System.out.println("WHAT THE FUCK???");
     }
 }
