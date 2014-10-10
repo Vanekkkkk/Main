4143a64042b32af404cd126dd918d85ffc866306
Vanekkkkk <semenov.vanya@gmail.com>
2014-10-10 03:38:34 +0300

    Fourth

--- FuckThisShit1/src/ShitSwagVipEdition/FUckThisShit2.java
+++ FuckThisShit1/src/ShitSwagVipEdition/FUckThisShit2.java
@@ -7,17 +7,21 @@ public class FUckThisShit2 {
         Scanner scan = new Scanner(System.in);
         Random random = new Random();
         System.out.println("Input a of [a;b], BITCH!");
-        if(scan.hasNextInt()){
+        if(!scan.hasNextInt()){
+            System.out.println("Wrong, BITCH!");
+        } else{
             a = scan.nextInt();
-        } else System.out.println("Wrong, BITCH!");
-        System.out.println("Input b of [a;b], BITCH!");
-        if(scan.hasNextInt()){
-            b = scan.nextInt();
-        } else System.out.println("Wrong, BITCH!");
-        if(a < b){
-            int c = random.nextInt(b - a) + a;
-            System.out.println("Your number of [a;b] is " + c);
-            System.out.println("WHERE IS MY MONEY, BITCH?!");
-        } else System.out.println("WHAT THE FUCK???");
+            System.out.println("Input b of [a;b], BITCH!");
+            if(!scan.hasNextInt()){
+                System.out.println("Wrong, BITCH!");
+            } else{
+                b = scan.nextInt();
+                if(a < b){
+                int c = random.nextInt(b - a) + a;
+                System.out.println("Your number of [a;b] is " + c);
+                System.out.println("WHERE IS MY MONEY, BITCH?!");
+                } else System.out.println("WHAT THE FUCK???");
+            }
+        }
     }
 }
